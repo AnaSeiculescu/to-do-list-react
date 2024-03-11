36f1f26e8d5e3052e4c6d8ed26bb6c14fe1a4101
Ana Seiculescu <anaseiculescu@gmail.com>
2024-03-11 09:05:23 +0200

    adding background image for the login page, and css file

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,97 +1,70 @@
-// import Box from '@mui/material/Box';
 import { Button } from '@mui/material';
 import TextField from '@mui/material/TextField';
-// import Button from '@mui/material/Button';
 import { useState } from 'react';
-// import React from 'react';
 // import { Link, Route, Routes, useNavigate } from 'react-router-dom';
 import { Route, Routes, useNavigate } from 'react-router-dom';
-import todoImage from '../../pictures/todo_image.jpeg';
+// import todoImage from '../../public/pictures/todo_image.jpeg';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
+import Grid from '@mui/material/Grid';
+import '../utils/StyleLoginPage.css';
 
 export function LoginPage() {
-    const styleLoginPage = {
-        backgroundImage: `url(${todoImage})`,
-        width: '100vw',
-        height: '100vh',
-        backgroundPosition: 'center',
-        backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat',
-        position: 'absolute',
-        top: '0',
-        left: '0',
-        filter: 'grayscale(30%) opacity(0.7) brightness(0.3)',
-    };
-
     const navigate = useNavigate();
 
     const [userNameInput, setUserNameInput] = useState('');
     const [passInput, setPassInput] = useState('');
 
     const inputsStyle = {
+        width: '85%',
         marginBottom: '15px',
+        display: 'block',
+        textAlign: 'center',
     };
 
     const handleInputChange = (event, properSetter) => {
         properSetter(event.target.value);
     };
 
-    // const handleOnClikHomeButton = () => {};
-
     return (
-        <div style={styleLoginPage}>
-            <Card
-                sx={{ maxWidth: 400 }}
-                style={{
-                    textAlign: 'center',
-                    margin: 'auto',
-                    filter: 'invert(0%)',
-                }}
-            >
-                <CardContent>
-                    <TextField
-                        required
-                        label="Username: required"
-                        // defaultValue="User Name"
-                        value={userNameInput}
-                        onChange={(event) =>
-                            handleInputChange(event, setUserNameInput)
-                        }
-                        style={inputsStyle}
-                    />
-                    <TextField
-                        required
-                        label="Password: required"
-                        // defaultValue="Password"
-                        type="password"
-                        value={passInput}
-                        onChange={(event) =>
-                            handleInputChange(event, setPassInput)
-                        }
-                        style={inputsStyle}
-                    />
-                </CardContent>
-
-                {/* <Link
-                    to="/home-page"
-                    // size="small"
-                    // onClick={handleOnClikHomeButton}
-                    // style={{
-                    //     margin: '5px 10px',
-                    //     // textTransform: 'none'
-                    // }}
+        <div className="styleLoginPage">
+            <Grid container>
+                <Card
+                    sx={{ width: 300, height: 500, boxShadow: 5 }}
+                    style={{
+                        textAlign: 'center',
+                        margin: 'auto',
+                    }}
                 >
-                    Home
-                </Link> */}
-                <Button
-                    variant="contained"
-                    onClick={() => navigate('/home-page')}
-                >
-                    Submit
-                </Button>
-            </Card>
+                    <CardContent>
+                        <TextField
+                            required
+                            label="Username: required"
+                            value={userNameInput}
+                            onChange={(event) =>
+                                handleInputChange(event, setUserNameInput)
+                            }
+                            style={inputsStyle}
+                        />
+                        <TextField
+                            required
+                            label="Password: required"
+                            type="password"
+                            value={passInput}
+                            onChange={(event) =>
+                                handleInputChange(event, setPassInput)
+                            }
+                            style={inputsStyle}
+                        />
+                    </CardContent>
+                    <Button
+                        variant="contained"
+                        onClick={() => navigate('/home-page')}
+                    >
+                        Submit
+                    </Button>
+                </Card>
+            </Grid>
 
             <Routes>
                 <Route
