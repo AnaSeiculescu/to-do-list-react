8c3ab99e71394d90e54711efb686672ade0ad415
Vallex <aharapu2@yahoo.com>
2024-07-23 17:56:09 +0300

    Update vite build asset file names

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+function getCurrentDateTime() {
+    const now = new Date();
+
+    const year = now.getFullYear();
+    const month = String(now.getMonth() + 1).padStart(2, '0'); // Months are zero-based
+    const day = String(now.getDate()).padStart(2, '0');
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+
+    return `${year}-${month}-${day}_${hours}-${minutes}`;
+}
+
+const timestamp = getCurrentDateTime();
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    build: {
+        rollupOptions: {
+            output: {
+                entryFileNames: `assets/[name].${timestamp}.js`,
+                chunkFileNames: `assets/[name].${timestamp}.js`,
+                assetFileNames: `assets/[name].${timestamp}.[ext]`,
+            },
+        },
+    },
+});
